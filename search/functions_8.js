10b802d35026ba4cf39f15a955fc72c3f7f44b98
Steven BOEHM <steven.boehm.dev@gmail.com>
2021-06-04 02:23:37 +0200

    add partition, uniqBy and splitAt functions

--- search/functions_8.js
+++ search/functions_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['some',['some',['../classboehm__s_1_1F.html#aee63ff04d9ecde792865a5435a6e8cc9',1,'boehm_s::F']]],
-  ['sort',['sort',['../classboehm__s_1_1F.html#a742910125ee71afd3149a16262506600',1,'boehm_s::F']]]
+  ['sort',['sort',['../classboehm__s_1_1F.html#a742910125ee71afd3149a16262506600',1,'boehm_s::F']]],
+  ['splitat',['splitAt',['../classboehm__s_1_1F.html#a7187437d20f86874a6c8115e0f8b3959',1,'boehm_s::F']]]
 ];
